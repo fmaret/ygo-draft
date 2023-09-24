3b1f54411090e0067e93803a413882862644f952
OverAngelix <OverAngelix@hotmail.fr>
2023-09-24 15:15:00 +0200

    Clear storage in menu

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -29,6 +29,11 @@
       >
         Export
       </button>
+
+      <button v-if="options" class="custom-button" @click="clearLocalStorage()">
+        Clear Local Storage
+      </button>
+
       <button v-if="options" class="custom-button" @click="goTo('/test')">
         Test
       </button>
@@ -86,6 +91,9 @@ export default {
       a.download = "ygo-draft-export.ydk";
       a.click();
       URL.revokeObjectURL(url);
+    },
+    clearLocalStorage() {
+      localStorage.clear();
     },
   },
 };
